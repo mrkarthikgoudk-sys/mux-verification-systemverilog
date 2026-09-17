579825e711fb4f6bcf3a5ee7f311dfb53a60f18d
mrkarthikgoudk-sys <mr.karthikgoudk@gmail.com>
2026-09-17 15:11:00 -0500

    Create mux.sv

---
+++ mux.sv
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+
+module mux (
+    input  logic a,
+    input  logic b,
+    input  logic sel,
+    output logic y
+);
+
+    always_comb begin
+        if (sel == 1'b0)
+            y = a;
+        else
+            y = b;
+    end
+
+endmodule
